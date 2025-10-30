68eb546e088f538aa9746ec69acdb5cb3c925617
necro50n3 <68177736+necro50n3@users.noreply.github.com>
2025-10-30 21:03:45 +1100

    Fixed rct ai and last resort

---
+++ common/src/main/java/com/necro/raid/dens/common/mixins/ai/RandomBattleAIMixin.java
@@ -0,0 +1,44 @@
+package com.necro.raid.dens.common.mixins.ai;
+
+import com.cobblemon.mod.common.battles.*;
+import com.cobblemon.mod.common.battles.ai.RandomBattleAI;
+import com.necro.raid.dens.common.util.IRaidBattle;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.*;
+
+@Mixin(RandomBattleAI.class)
+public abstract class RandomBattleAIMixin {
+    @Inject(method = "choose", at = @At("HEAD"), remap = false, cancellable = true)
+    private void chooseInject(ActiveBattlePokemon pokemon, ShowdownMoveset moveset, boolean forceSwitch, CallbackInfoReturnable<ShowdownActionResponse> cir) {
+        if (!((IRaidBattle) pokemon.getBattle()).isRaidBattle()) return;
+        else if (moveset == null) {
+            cir.setReturnValue(PassActionResponse.INSTANCE);
+            return;
+        }
+
+        List<InBattleMove> filteredMoves = new ArrayList<>(moveset.getMoves().stream()
+            .filter(InBattleMove::canBeUsed)
+            .filter(move -> move.mustBeUsed() || !move.getTarget().getTargetList().invoke(pokemon).isEmpty())
+            .filter(move -> !move.id.equals("lastresort"))
+            .toList());
+        if (filteredMoves.isEmpty()) cir.setReturnValue(new MoveActionResponse("struggle", null, null));
+
+        Collections.shuffle(filteredMoves);
+        InBattleMove bestMove = filteredMoves.getFirst();
+
+        List<Targetable> target = bestMove.mustBeUsed() ? null
+            : new ArrayList<>(bestMove.getTarget().getTargetList().invoke(pokemon).stream().filter(t -> !t.isAllied(pokemon)).toList());
+        if (target == null) {
+            cir.setReturnValue(new MoveActionResponse(bestMove.id, null, null));
+        }
+        else {
+            Collections.shuffle(target);
+            ActiveBattlePokemon chosenTarget = (ActiveBattlePokemon) target.getFirst();
+            cir.setReturnValue(new MoveActionResponse(bestMove.id, chosenTarget.getPNX(), null));
+        }
+    }
+}
---
+++ fabric/src/main/java/com/necro/raid/dens/fabric/mixins/ai/RCTBattleAIMixin.java
@@ -0,0 +1,27 @@
+package com.necro.raid.dens.fabric.mixins.ai;
+
+import com.cobblemon.mod.common.battles.*;
+import com.cobblemon.mod.common.battles.pokemon.BattlePokemon;
+import com.gitlab.srcmc.rctapi.api.ai.RCTBattleAI;
+import com.necro.raid.dens.common.compat.rctapi.RCTBattleAIImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RCTBattleAI.class)
+public abstract class RCTBattleAIMixin {
+    @Shadow(remap = false)
+    protected abstract double evalMove(BattlePokemon from, BattlePokemon to, InBattleMove move);
+
+    @Inject(method = "choose", at = @At("HEAD"), remap = false, cancellable = true)
+    private void chooseInject(ActiveBattlePokemon pokemon, ShowdownMoveset moveset, boolean forceSwitch, CallbackInfoReturnable<ShowdownActionResponse> cir) {
+        RCTBattleAIImpl.choose(pokemon, moveset, this::evalMove, cir);
+    }
+
+    @Inject(method = "evalMove", at = @At("HEAD"), remap = false, cancellable = true)
+    private void evalMoveInject(BattlePokemon from, BattlePokemon _to, InBattleMove move, CallbackInfoReturnable<Double> cir) {
+        RCTBattleAIImpl.evalMove(move, cir);
+    }
+}
