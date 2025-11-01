73322d41fd95b6eaccd6623b14b92e5bff6d9301
necro50n3 <68177736+necro50n3@users.noreply.github.com>
2025-11-01 22:36:18 +1100

    Added pokemon field to raid end event

--- common/src/main/java/com/necro/raid/dens/common/events/RaidEndEvent.java
+++ common/src/main/java/com/necro/raid/dens/common/events/RaidEndEvent.java
@@ -1,5 +1,6 @@
 package com.necro.raid.dens.common.events;
 
+import com.cobblemon.mod.common.pokemon.Pokemon;
 import com.necro.raid.dens.common.raids.RaidBoss;
 import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.NotNull;
@@ -7,11 +8,13 @@ import org.jetbrains.annotations.NotNull;
 public class RaidEndEvent {
     private final ServerPlayer player;
     private final RaidBoss raidBoss;
+    private final Pokemon pokemon;
     private final boolean isWin;
 
-    public RaidEndEvent(@NotNull ServerPlayer player, @NotNull RaidBoss raidBoss, boolean isWin) {
+    public RaidEndEvent(@NotNull ServerPlayer player, @NotNull RaidBoss raidBoss, Pokemon pokemon, boolean isWin) {
         this.player = player;
         this.raidBoss = raidBoss;
+        this.pokemon = pokemon;
         this.isWin = isWin;
     }
 
@@ -21,6 +24,10 @@ public class RaidEndEvent {
 
     public RaidBoss getRaidBoss() {
         return this.raidBoss;
+    }
+
+    public Pokemon getPokemon() {
+        return this.pokemon;
     }
 
     public boolean isWin() {
