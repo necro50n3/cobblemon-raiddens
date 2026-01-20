8484ac0ab50aadedcc532ff60266b7ed74557af4
necro50n3 <68177736+necro50n3@users.noreply.github.com>
2026-01-20 12:42:18 +1100

    Fixed script config

--- common/src/main/java/com/necro/raid/dens/common/data/adapters/ScriptAdapter.java
+++ common/src/main/java/com/necro/raid/dens/common/data/adapters/ScriptAdapter.java
@@ -1,14 +1,10 @@
 package com.necro.raid.dens.common.data.adapters;
 
-import com.google.gson.JsonParser;
 import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.JsonOps;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Jankson;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.JsonElement;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.JsonObject;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.api.SyntaxError;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record ScriptAdapter(List<String> functions) {
@@ -21,18 +17,33 @@ public record ScriptAdapter(List<String> functions) {
                 ScriptAdapter::new,
                 ScriptAdapter::new
             ),
-            script -> Either.right(script.functions)
+            script -> {
+                if (script.functions.size() == 1) return Either.left(script.functions.getFirst());
+                return Either.right(script.functions);
+            }
         );
 
     public JsonElement serialize() {
-        try {
-            return Jankson.builder().build().loadElement(CODEC.encodeStart(JsonOps.INSTANCE, this).getOrThrow().toString());
-        } catch (SyntaxError e) {
-            return new JsonObject();
-        }
+        if (this.functions().size() == 1) return new JsonPrimitive(this.functions().getFirst());
+        JsonArray array = new JsonArray();
+        this.functions().forEach(function -> array.add(new JsonPrimitive(function)));
+        return array;
     }
 
     public static ScriptAdapter deserialize(JsonElement json) {
-        return CODEC.decode(JsonOps.INSTANCE, JsonParser.parseString(json.toJson())).getOrThrow().getFirst();
+        switch (json) {
+            case JsonPrimitive primitive -> {
+                return new ScriptAdapter((String) primitive.getValue());
+            }
+            case JsonArray array -> {
+                List<String> functions = new ArrayList<>();
+                for (JsonElement element : array) {
+                    if (!(element instanceof JsonPrimitive primitive)) continue;
+                    functions.add((String) primitive.getValue());
+                }
+                return new ScriptAdapter(functions);
+            }
+            default -> throw new IllegalArgumentException("Expected string or string array for Script, got: " + json);
+        }
     }
 }
