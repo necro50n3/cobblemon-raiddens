4d23229202c9868fc8712e2c3eb015524948411f
necro50n3 <68177736+necro50n3@users.noreply.github.com>
2026-02-08 19:57:12 +1100

    Fixed raid buckets not working for raids generated without buckets

--- common/src/main/java/com/necro/raid/dens/common/blocks/entity/RaidCrystalBlockEntity.java
+++ common/src/main/java/com/necro/raid/dens/common/blocks/entity/RaidCrystalBlockEntity.java
@@ -134,6 +134,7 @@ public abstract class RaidCrystalBlockEntity extends BlockEntity implements GeoB
     }
 
     private ResourceLocation generateFromBucket(Level level, BlockState blockState, RaidCycleMode cycleMode) {
+        if (this.raidBucket == null) this.raidBucket = RaidBucketRegistry.getRandomBucket(level.getRandom(), level.getBiome(this.getBlockPos()));
         if (this.raidBucket == null) return this.generateRandom(level, blockState, cycleMode);
         ResourceLocation boss = RaidBucketRegistry.getBucket(this.raidBucket).getRandomRaidBoss(level.getRandom(), level);
         return boss == null ? this.generateRandom(level, blockState, cycleMode) : boss;
