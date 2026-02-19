0a619b2e38d9214aca3a15e923cf605568e63e54
Kaividian <kaividian@gmail.com>
2026-02-19 22:03:03 +1100

    ComponentUtils - added fromJsonText method for creating components from a json string

--- common/src/main/java/com/necro/raid/dens/common/util/ComponentUtils.java
+++ common/src/main/java/com/necro/raid/dens/common/util/ComponentUtils.java
@@ -1,7 +1,12 @@
 package com.necro.raid.dens.common.util;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.mojang.serialization.JsonOps;
+
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentSerialization;
 import net.minecraft.network.chat.MutableComponent;
 
 public class ComponentUtils {
@@ -18,5 +23,12 @@ public class ComponentUtils {
 
     public static Component getErrorMessage(MutableComponent component) {
         return component.withStyle(ChatFormatting.RED);
+    }
+
+    public static Component fromJsonText(String jsonString) {
+        return ComponentSerialization.CODEC
+		.decode(JsonOps.INSTANCE, new Gson().fromJson(jsonString, JsonElement.class))
+		.getOrThrow().
+		.getFirst();
     }
 }
