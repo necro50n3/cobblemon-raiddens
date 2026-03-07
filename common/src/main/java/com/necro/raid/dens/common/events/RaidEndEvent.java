6d18f80b895e2d5e47e5d04ee5fdf7b6420b00f9
necro50n3 <68177736+necro50n3@users.noreply.github.com>
2026-03-07 18:24:54 +1100

    Loot table buffs

--- common/src/main/java/com/necro/raid/dens/common/events/RaidEndEvent.java
+++ common/src/main/java/com/necro/raid/dens/common/events/RaidEndEvent.java
@@ -3,7 +3,6 @@ package com.necro.raid.dens.common.events;
 import com.cobblemon.mod.common.pokemon.Pokemon;
 import com.necro.raid.dens.common.data.raid.RaidBoss;
 import net.minecraft.server.level.ServerPlayer;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public record RaidEndEvent(ServerPlayer player, RaidBoss raidBoss, @Nullable Pokemon pokemon, float catchRate, boolean isWin) {}
