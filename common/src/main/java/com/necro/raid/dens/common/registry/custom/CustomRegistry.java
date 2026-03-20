826da1d0c705ea6a4f9c0c152e2047ad368021de
necro50n3 <68177736+necro50n3@users.noreply.github.com>
2026-03-20 22:50:06 +1100

    Added extra registry utility

--- common/src/main/java/com/necro/raid/dens/common/registry/custom/CustomRegistry.java
+++ common/src/main/java/com/necro/raid/dens/common/registry/custom/CustomRegistry.java
@@ -4,7 +4,9 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Optional;
 
+@SuppressWarnings("unused")
 public class CustomRegistry<K, T> {
     private final Object2ObjectOpenHashMap<K, T> registry;
     private boolean frozen;
@@ -18,6 +20,7 @@ public class CustomRegistry<K, T> {
 
     public void register(K id, T entry) {
         if (this.frozen) throw new IllegalStateException("Attempted to add to registry after initialization.");
+        else if (id == null) throw new IllegalArgumentException("Attempted to register a null key");
         this.registry.put(id, entry);
     }
 
@@ -28,6 +31,18 @@ public class CustomRegistry<K, T> {
 
     public T get(K key) {
         return this.registry.getOrDefault(key, this.registry.get(this.defaultKey));
+    }
+
+    public Optional<T> getOptional(K key) {
+        return Optional.ofNullable(this.registry.get(key));
+    }
+
+    public boolean containsKey(K key) {
+        return this.registry.containsKey(key);
+    }
+
+    public Collection<K> keySet() {
+        return Collections.unmodifiableCollection(this.registry.keySet());
     }
 
     public Collection<T> values() {
