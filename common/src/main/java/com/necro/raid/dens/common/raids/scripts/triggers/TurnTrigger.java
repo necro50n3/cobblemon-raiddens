f531b53a82e49f4207e05d55963f8cdf95d1c57e
necro50n3 <68177736+necro50n3@users.noreply.github.com>
2026-04-03 12:28:07 +1100

    Added turn_repeat trigger

--- common/src/main/java/com/necro/raid/dens/common/raids/scripts/triggers/TurnTrigger.java
+++ common/src/main/java/com/necro/raid/dens/common/raids/scripts/triggers/TurnTrigger.java
@@ -7,14 +7,21 @@ import java.util.List;
 
 public class TurnTrigger extends RaidTrigger<Integer> {
     private final int turn;
+    private final boolean repeat;
 
-    public TurnTrigger(int turn, List<AbstractEvent> events) {
+    public TurnTrigger(int turn, boolean repeat, List<AbstractEvent> events) {
         super(RaidTriggerType.TURN, events);
-        this.turn = turn;
+        this.turn = Math.max(1, turn);
+        this.repeat = repeat;
     }
 
     @Override
     protected boolean check(Integer predicate) {
-        return this.turn == predicate;
+        return predicate % this.turn == 0;
+    }
+
+    @Override
+    protected boolean shouldExpire() {
+        return !this.repeat;
     }
 }
