c4c98663f20d5d2b45c6898be8f3318e29e3a3b8
necro50n3 <68177736+necro50n3@users.noreply.github.com>
2026-05-28 20:57:02 +1000

    Added a command to refresh all raids when reset mode is global

--- common/src/main/java/com/necro/raid/dens/common/data/raid/RaidTimerMode.java
+++ common/src/main/java/com/necro/raid/dens/common/data/raid/RaidTimerMode.java
@@ -4,5 +4,9 @@ public enum RaidTimerMode {
     GAME_TIME,
     SYSTEM_TIME,
     GLOBAL_GAME_TIME,
-    GLOBAL_SYSTEM_TIME
+    GLOBAL_SYSTEM_TIME;
+
+    public boolean isGlobal() {
+        return this == GLOBAL_GAME_TIME || this == GLOBAL_SYSTEM_TIME;
+    }
 }
