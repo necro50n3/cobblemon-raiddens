bbb2cd547e5cd113bb53fbe1b5ad62be850eb83e
necro50n3 <68177736+necro50n3@users.noreply.github.com>
2026-08-02 20:29:13 +1000

    Fixed resetclears command not working with command blocks

--- common/src/main/java/com/necro/raid/dens/common/commands/RaidAdminCommands.java
+++ common/src/main/java/com/necro/raid/dens/common/commands/RaidAdminCommands.java
@@ -48,7 +48,6 @@ public class RaidAdminCommands {
                                 .executes(RaidAdminCommands::resetClearsForPlayerAndPos)
                             )
                         )
-                        .requires(CommandSourceStack::isPlayer)
                         .executes(context -> resetClearsForAll(
                             context, BlockPosArgument.getBlockPos(context, "pos")
                         ))
